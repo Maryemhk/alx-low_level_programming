6ebdfbf0d8b709e67577c7e9d1b583fdd50a9107
gide lolo <gide@gmail.com>
2023-03-16 17:19:35 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main - A program that prints a line with put function 
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a mulilingual puzzle");
+return (0);
+}
+

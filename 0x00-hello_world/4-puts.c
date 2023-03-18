6ab32f4246b126e6dc13c1c17880ace4e2082289
gide lolo <gide@gmail.com>
2023-03-17 18:21:46 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
 *   main - prints "Programming is like building a multilingual
 *   puzzle, followed by newline.");
-*   return (0);
+*   Return: Always 0.
 */
 
 int main(void)
 
 {
-	puts("\"Pogrmaming is like building a multilingual puzzle ");
+	puts("\"Pogrmaming is like building a multilingual puzzle");
 	return(0);
 }
